9c2f32de18ff611862d965f293ce7b1017fd88b0
Charlie Weber <charlestweber@gmail.com>
2021-10-15 11:26:13 -0700

    add Factory summary to Home splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
       public ActionResult Index()
       {
-        return View();
+        Dictionary <string, object> model = new Dictionary <string, object>();
+        var Engineers = _db.Engineers.ToList();
+        var Machines = _db.Machines.ToList();
+        model.Add("Engineers", Engineers);
+        model.Add("Machines", Machines);
+        return View(model);
       }
     }
 }
